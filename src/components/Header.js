047eb0d11fac5483002aac8e556b074199cefc66
simon <vjp96awesomecreations@gmail.com>
2020-11-08 21:08:48 +0530

    rewrited !

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,38 +12,40 @@ export default function Header() {
   const styleTranslate = 'translate-y-10 opacity-0';
   const animated = useStartAnimation();
   return (
+
     <div
       style={{
+
         background: '#091C29',
+
       }}
       className="min-h-screen flex items-center justify-center"
       id="header"
     >
       <div className="flex flex-col items-center justify-center md:flex-row-reverse md:w-10/12 md:justify-between">
-        <div className="w-full md:w-2/5 ">
+        <div className="w-full ">
           <LazyLoadImage
-            src={content.header.img}
+
             alt="profile"
             className={`w-full mx-auto`}
             effect="blur"
-            placeholderSrc={content.header.imgPlaceholder}
+
           />
         </div>
 
-        <div className="font-dosis w-full md:w-3/5 text-center md:text-left ">
+        <div className=" flex flex-col items-center justify-center w-full " >
           <h2
-            className={`text-3xl md:text-4xl lg:text-6xl text-white font-bold transform ${
-              animated ? 'translate-y-0' : styleTranslate
-            }  ${transition(2000)} `}
+            className={`text-3xl md:text-4xl lg:text-6xl text-white font-bold transform ${animated ? 'translate-y-0' : styleTranslate
+              }  ${transition(2000)} `}
           >
             {content.header.text[0]}
+            <br />
             <br />
             {content.header.text[1]}
           </h2>
           <h1
-            className={`text-2xl md:text-4xl text-gray-400 transform ${
-              animated ? 'translate-y-0' : styleTranslate
-            } ${transition(3000)} `}
+            className={`text-2xl md:text-4xl text-gray-400 transform ${animated ? 'translate-y-0' : styleTranslate
+              } ${transition(3000)} `}
           >
             {content.header.text[2]}
             <Typical
@@ -56,11 +58,9 @@ export default function Header() {
 
           <ScrollLink to="mywork" smooth={true}>
             <button
-              className={` animate-bounce bg-indigo-500 px-10 py-3 text-lg uppercase text-white rounded-lg mt-10 hover:bg-indigo-300 transform  ${
-                animated ? 'translate-y-0' : styleTranslate
-              } ${transition(3500)}`}
+              className="  bg-indigo-500 px-10 py-3 text-lg uppercase text-white rounded-lg mt-10 hover:bg-indigo-300 transform"
             >
-              {content.header.btnText}
+              Discover More
             </button>
           </ScrollLink>
         </div>
--- src/components/Stack.js
+++ src/components/Stack.js
@@ -15,16 +15,7 @@ export default function Stack() {
       <h1 className="text-5xl font-dosis font-bold mb-10">Stack I Use</h1>
       <div>
         <div className="flex flex-wrap justify-center">
-          {content.stack.tech.map((tech, index) => (
-            <span
-              key={index}
-              className={` h-40 w-40 bg-white shadow-xl  ml-2  rounded-full flex justify-center items-center p-5 m-2 ${
-                index % 2 !== 0 ? 'animate-bounce' : 'animate-bounceFirst'
-              }`}
-            >
-              <LazyLoadImage effect="blur" src={tech.img} alt={tech.alt} />
-            </span>
-          ))}
+
         </div>
       </div>
       <p className="mt-10 mb-10 w-11/12 md:max-w-xl text-center inline-block text-xl md:text-2xl font-dosis ">
